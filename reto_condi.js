83c07f3f8a1941d756d8546ae9adbbc1c311c9d8
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-05-27 16:50:32 -0500

    Condicional sobre salario bruto, deducciones, bonificaciones y salario neto

--- reto_condi.js
+++ reto_condi.js
@@ -1,40 +1,34 @@
 function aniadirTexto() {
-    var texto = document.getElementById("texto");
-    var mostrar = document.getElementById("mostrar_texto");
-
-    mostrar.innerHTML = texto.value;
+    let text_input = document.getElementById("texto");
+    let text_output = document.getElementById("mostrar_texto");
+    text_output.innerHTML = text_input.value;
+    
 }
-function enviarNums() {
-    var num_one = parseFloat(document.getElementById("numero_1").value);
-    var num_two = parseFloat(document.getElementById("numero_2").value);
-    var num_three = parseFloat(document.getElementById("numero_3").value)
+function enviarDts() {
+    var name_t = document.getElementById("nombre").value;
+    var salBH = parseInt(document.getElementById("salarioHora").value);
+    var hours = parseInt(document.getElementById("horas").value);
     var retornar = document.getElementById("retornar_texto");
 
-    //Hacer una solución que lea tres números y luego los imprima ascendentemente.
-
-    if (num_one > num_two && num_one > num_three) {
-        if (num_two > num_three) {
-            retornar.innerHTML = retornar.innerHTML = num_one + " " + num_two + " " + num_three
-        }else {
-            retornar.innerHTML = num_one + " " + num_three + " " + num_two
-        }
-    }else {
-        if (num_two > num_one && num_two > num_three) {
-            if (num_one > num_three) {
-                retornar.innerHTML = num_two + " " + num_one + " " + num_three
-            } else {
-                retornar.innerHTML = num_two + " " + num_three + " " + num_one
-            }
-        } else {
-            if (num_three > num_one && num_three > num_two) {
-                if (num_one > num_two) {
-                    retornar.innerHTML = num_three + " " + num_one + " " + num_two
-                } else {
-                    retornar.innerHTML = num_three + " " + num_two + " " + num_one
-                }
-                
-            }
-        }
+var salBruT = salBH * hours;
+var salNet = 0;
+if (hours <= 48) {
+    salNet += salBruT + 20000;
+} else {
+    if ( hours <= 58){ 
+        salNet += salBruT + 50000;
+    } else {
+        salNet += salBruT + 100000;
     }
-
+}
+if (salBH <= 5000) {
+    salNet -= 10000;
+}else {
+    if(salBH < 8000) {
+        salNet -= 20000;
+    } else {
+        salNet -= 50000;
+    }
+}
+retornar.innerHTML = name_t + " el salario bruto es " + salBruT + "el salario neto es: " + salNet   ;
 }
