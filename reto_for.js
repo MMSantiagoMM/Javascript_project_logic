a333aefeeefdcf9840b962d651f54a9ba491d24d
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-05-27 19:20:55 -0500

    Ciclo for no finalizado

--- reto_for.js
+++ reto_for.js
@@ -5,14 +5,16 @@ function aniadirTexto() {
 }
 function enviarDTS() {
     let num = parseInt(document.getElementById("numero").value);
-    let salida = document.getElementById("retornar")
-    let serie =4;
-    //    1. Elaborar un diagrama que imprima los primeros 15 términos de la siguiente serie de números:
-    //4 - 8 - 12 - 16 - 20 - 24 .........
-    for (let i = 0; i < num; i++){
-        salida.innerHTML += serie + " ";
-        serie += 4;
+    let name = document.getElementById("nombre").value;
+    let salida = document.getElementById("retornar");
 
+    //    Elabore un diagrama que lea para un grupo de 15 personas el nombre y la edad, e imprima por cada una el nombre y un mensaje que imprima si es mayor o menor de edad.
+    for (let i = 0; i < 2; i++) {
+        if (num >= 18) {
+            salida.innerHTML += name + " es mayor de edad"
+        }else {
+            salida.innerHTML += name + " no es mayor de edad"
+        }
     }
     
         
